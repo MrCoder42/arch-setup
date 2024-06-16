919550e485d4d8ba7d52a4e835b9e386eb6fe213
Ashish Rai <ashishrai@duck.com>
2024-06-16 15:55:36 +0530

    Init splash screen for arch setup

--- setup.sh
+++ setup.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+# This is an entry point to the installer which executes subprograms
+# during various stages of setup.
+
+set -ae
+
+echo -ne"
+
+----------------------------------------------------------------------------------------------------
+				ARCH LINUX SETUP | MrCoder42
+----------------------------------------------------------------------------------------------------
+
+"
